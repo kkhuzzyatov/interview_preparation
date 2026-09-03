59c6774e6bce7e0c3bd37208c3de7f4e2d34d0c5
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-09-03 19:06:44 +0300

    feat: add TimeConfig

---
+++ backend/src/main/kotlin/com/backend/config/TimeConfig.kt
@@ -0,0 +1,11 @@
+package com.backend.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.Clock
+
+@Configuration
+class TimeConfig {
+    @Bean
+    fun clock(): Clock = Clock.systemUTC()
+}
