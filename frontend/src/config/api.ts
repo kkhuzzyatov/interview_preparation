201f5bca798ce3ee5c482c6cdc97386d261d506d
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-09-05 20:23:14 +0300

    fix: fix hardcode of server uri

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080";
+  import.meta.env.VITE_API_BASE_URL ?? "http://94.103.3.251:8080";
 
 export const API_ENDPOINTS = {
   auth: {
